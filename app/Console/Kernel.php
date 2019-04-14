9075cf875b3a184cca4f6201b75fcd59f7c81e49
InFeCtedEv <roscher794@gmail.com>
2019-04-14 05:35:41 +0200

    Project creation

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+//        test
         // $schedule->command('inspire')
         //          ->hourly();
     }
